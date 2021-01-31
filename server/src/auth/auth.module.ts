32d0904595a1146cac016477ac2116a913611de8
dkd7142@naver,com <dkd7142@naver.com>
2021-02-01 00:28:02 +0900

    #Feature jwt 전략 추가

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 import { LocaStrategy } from './local.strategy';
 import { AuthResolver } from './auth.resolver';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { AuthResolver } from './auth.resolver';
       signOptions: { expiresIn: '1d' },
     }),
   ],
-  providers: [AuthResolver, AuthService, LocaStrategy],
+  providers: [AuthResolver, AuthService, LocaStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
