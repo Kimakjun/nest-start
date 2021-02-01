0c7bbcc40338b31e075d422638624f70e0444406
dkd7142@naver,com <dkd7142@naver.com>
2021-02-01 12:09:36 +0900

    #Feat class-validator email, length 검사추가

--- server/src/User/user.schema.ts
+++ server/src/User/user.schema.ts
@@ -1,7 +1,7 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { IsEmail } from 'class-validator';
+import { IsEmail, MaxLength, MinLength } from 'class-validator';
 
 @InputType('inputUser', { isAbstract: true })
 @ObjectType()
@@ -14,10 +14,13 @@ export class User {
   @Prop({ default: 'User' })
   name?: string;
 
+  @IsEmail()
   @Field((type) => String)
   @Prop({ required: true, unique: true })
   email: string;
 
+  @MinLength(4)
+  @MaxLength(20)
   @Field((type) => String)
   @Prop({ required: true })
   password: string;
