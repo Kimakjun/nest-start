1b21c79c927c67c5aca994155d259709d65c7cbb
dkd7142@naver,com <dkd7142@naver.com>
2021-02-06 22:15:05 +0900

    #Feature 파일업로드 리졸버 작성

--- server/src/file/file.resolver.ts
+++ server/src/file/file.resolver.ts
@@ -1,14 +1,19 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { GraphQLUpload, FileUpload } from 'graphql-upload';
 import { AddFileResponse } from './dto/addFile.dto';
+import { StorageService } from 'src/storage/storage.service';
 
 @Resolver()
 export class FileResolver {
+  constructor(private readonly strageService: StorageService) {}
+
   @Mutation((returns) => AddFileResponse)
   async uploadFile(
     @Args({ name: 'file', type: () => GraphQLUpload }) file: FileUpload,
   ): Promise<AddFileResponse> {
-    console.log(file);
-    return { result: 'true' };
+    const ext = file.filename.match(/\.[a-zA-Z]+$/);
+    const filename = `${new Date().getTime().toString()}${ext}`;
+    const url = await this.strageService.upload({ ...file, filename });
+    return { result: 'success', url: url };
   }
 }
