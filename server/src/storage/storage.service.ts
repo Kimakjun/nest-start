5da78a63115408045888c2b2b4303c41fbfdff95
dkd7142@naver,com <dkd7142@naver.com>
2021-02-06 22:13:52 +0900

    #Feature aws-adk 설치 및 s3 이미지 업로드 구현

--- server/src/storage/storage.service.ts
+++ server/src/storage/storage.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
+import { FileUpload } from 'graphql-upload';
 
 @Injectable()
-export class StorageService {}
+export class StorageService {
+  private s3: AWS.S3;
+  constructor() {
+    this.s3 = new AWS.S3({
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_KEY_ACCESS_ID,
+    });
+  }
+
+  async upload({ createReadStream, filename }: FileUpload): Promise<string> {
+    const stream = createReadStream();
+    const result = await this.s3
+      .upload({ Bucket: process.env.BUCKET_NAME, Key: filename, Body: stream })
+      .promise();
+    return result.Location;
+  }
+}
